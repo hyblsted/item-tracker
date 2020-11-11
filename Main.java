6a5cfbd32cdf95b5a65587df2be6e963a64fffc7
Alexander Menzel <alexander@menzel.dk>
2020-11-11 14:52:35 +0100

    class diagram

--- Main.java
+++ Main.java
@@ -4,3 +4,33 @@ public class Main{
         System.out.println();
     }
 }
+
+/*
+** PROGRAM SHOULD HAVE
+**
+** --Class: Item
+** --Fields:
+** String label
+** String description
+** String placement
+** int timestamp
+** --Contructor(String label, String description, String placement)
+** sets all fields + timestamp
+** --Methods:
+** getLabel()
+** getDescription()
+** getPlacement()
+** getDate()
+** setLabel()
+** setDescription()
+** setPlacement()
+
+** Class: ItemTracker
+** --Fields:
+** HashMap<String, Item> items
+** --Methods
+** getItemByLabel()
+** getAllItems()
+** addItem()
+** removeItem()
+*/
